7512c43d2dfc4e8ec7c1bc8f7cf3e27756d515d9
toannguyen112 <nguyencongtoana1nhvt@gmail.com>
2022-12-27 08:51:59 +0700

    make it better

--- src/App.js
+++ src/App.js
@@ -6,14 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="text-3xl text-primary font-bold underline">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
